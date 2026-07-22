b5fe82925e880090acd0450dc5dd30ed7dc57b0d
Aftab Fazal Qayum <ak375456@gmail.com>
2026-07-22 16:25:31 +0500

    pushed

--- android/app/src/main/kotlin/com/ibraheem/zabtec_scholarship/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ibraheem.zabtec_scholarship
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/src/services/receipt_pdf_saver.dart
+++
@@ -1,3 +0,0 @@
-export 'receipt_pdf_saver_stub.dart'
-    if (dart.library.io) 'receipt_pdf_saver_io.dart'
-    if (dart.library.html) 'receipt_pdf_saver_web.dart';
